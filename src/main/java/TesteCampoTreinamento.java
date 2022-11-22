9dfd6a514bb4c820df91f343b980fb974bf1d0b3
Clovis <clobnjuh@hotmail.com>
2022-11-22 11:33:44 -0300

    26 radio e check ok

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -10,13 +10,43 @@ public class TesteCampoTreinamento {
 	public void testeTextField() {
 		WebDriver driver = new FirefoxDriver();
 //		driver.manage().window().setSize(new Dimension(1200, 765));
-//		FirefoxOptions options = new FirefoxOptions();
-//		options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
 		
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste de escrita");
 		driver.findElement(By.id("elementosForm:nome")).getAttribute("value");
 		Assert.assertEquals("Teste de escrita", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void deveInteragirComTextArea() {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Teste\nQuebra de linha\n\nQuebra dupla");
+		Assert.assertEquals("Teste", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void deveInteragirComRaadioButton() {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+		
+		driver.quit();
+	}
+	
+	@Test
+	public void deveInteragirComCheckbox() {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
 		
 		driver.quit();
 	}
